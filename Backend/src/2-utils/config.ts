80bbc528d92c4f2f2c841d9b80ad54da2fd4fb6d
Niv Yulevitch <github@niv-yul.me>
2024-11-11 19:52:13 +0200

    updating the uri address

--- Backend/src/2-utils/config.ts
+++ Backend/src/2-utils/config.ts
@@ -1,5 +1,5 @@
 class Config {
-    public connectionString = "mongodb+srv://mongodbcom:03vkHeJ4aqrRZ8De@shopping-online.jwqcg.mongodb.net/?retryWrites=true&w=majority&appName=Shopping-Online"; //* Fill in the blank (database name)...
+    public connectionString = "mongodb+srv://mongodbcom:03vkHeJ4aqrRZ8De@shopping-online.jwqcg.mongodb.net/Shopping-Online?retryWrites=true&w=majority&appName=Shopping-Online"; //* Fill in the blank (database name)...
     public port = 3001;
 }
 
