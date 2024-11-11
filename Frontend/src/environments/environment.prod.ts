99fc4d3abd123e2749fbd7b1fc0deb32efa77eb2
Niv Yulevitch <github@niv-yul.me>
2024-11-11 20:06:39 +0200

    updating the environments

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -2,24 +2,24 @@ export const environment = {
     production: true,
     
     // auth: 
-    emailAndIdNumberUniqueUrl: 'http://localhost:3001/api/auth/check-if-email-or-id-is-unique/',
-    registerUrl: 'http://localhost:3001/api/auth/register/',
-    loginUrl: 'http://localhost:3001/api/auth/login/',
+    emailAndIdNumberUniqueUrl: 'https://shopping-online-xzr1.onrender.com/api/auth/check-if-email-or-id-is-unique/',
+    registerUrl: 'https://shopping-online-xzr1.onrender.com/api/auth/register/',
+    loginUrl: 'https://shopping-online-xzr1.onrender.com/api/auth/login/',
 
     // categories: 
-    categoriesUrl: 'http://localhost:3001/api/categories/',
+    categoriesUrl: 'https://shopping-online-xzr1.onrender.com/api/categories/',
 
     // products: 
-    productsUrl: 'http://localhost:3001/api/products/',
-    productsImageUrl: 'http://localhost:3001/shopping/images/',
+    productsUrl: 'https://shopping-online-xzr1.onrender.com/api/products/',
+    productsImageUrl: 'https://shopping-online-xzr1.onrender.com/shopping/images/',
 
     //carts
-    cartByUserUrl: 'http://localhost:3001/api/cart-by-user/',  //to display when user's cart was createdAt 
+    cartByUserUrl: 'https://shopping-online-xzr1.onrender.com/api/cart-by-user/',  //to display when user's cart was createdAt 
 
     // cart items 
-    cartItemsUrl: 'http://localhost:3001/api/items/',
-    cartItemsByCartUrl: 'http://localhost:3001/api/items-by-cart/',  //to display all items in user's current cart
+    cartItemsUrl: 'https://shopping-online-xzr1.onrender.com/api/items/',
+    cartItemsByCartUrl: 'https://shopping-online-xzr1.onrender.com/api/items-by-cart/',  //to display all items in user's current cart
 
     //  orders: 
-    ordersUrl: 'http://localhost:3001/api/orders/'
+    ordersUrl: 'https://shopping-online-xzr1.onrender.com/api/orders/'
 };
